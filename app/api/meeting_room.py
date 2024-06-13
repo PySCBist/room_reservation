5cd4ad700aba9039ce69005075ad39489441c2d1
Shmarev Dmitry <shds-2111@yandex.ru>
2024-06-14 02:31:05 +0300

    depends injection для использования сессии + получение всех переговорок

--- app/api/meeting_room.py
+++ app/api/meeting_room.py
@@ -1,20 +1,35 @@
-from fastapi import APIRouter, HTTPException
+from fastapi import APIRouter, HTTPException, Depends
 
-from app.schemas.meeting_room import MeetingRoomCreate
-from app.crud.meeting_room import create_meeting_room, get_room_id_by_name
+from sqlalchemy.ext.asyncio import AsyncSession
+
+from app.core.db import get_async_session
+from app.schemas.meeting_room import MeetingRoomCreate, MeetingRoomDB
+from app.crud.meeting_room import create_meeting_room, get_room_id_by_name, read_all_rooms_from_db
 
 
 router = APIRouter()
 
 
-@router.post('/meeting_rooms/')
+@router.post('/meeting_rooms/',
+             response_model=MeetingRoomDB,
+             response_model_exclude_none=True)
 async def create_new_meeting_room(
-        meeting_room: MeetingRoomCreate
+        meeting_room: MeetingRoomCreate,
+        session: AsyncSession = Depends(get_async_session)
 ):
-    room_id = get_room_id_by_name(meeting_room.name)
+    room_id = await get_room_id_by_name(meeting_room.name, session)
     if room_id is not None:
         raise HTTPException(
             status_code=422,
             detail='Переговорка с таким именем уже существует!')
-    new_room = await create_meeting_room(meeting_room)
+    new_room = await create_meeting_room(meeting_room, session)
     return new_room
+
+
+@router.get('/meeting_rooms/',
+            response_model=list[MeetingRoomDB],
+            response_model_exclude_none=True)
+async def get_all_meeting_rooms(
+        session: AsyncSession = Depends(get_async_session)):
+    all_rooms = await read_all_rooms_from_db(session)
+    return all_rooms
--- app/crud/meeting_room.py
+++ app/crud/meeting_room.py
@@ -1,30 +1,37 @@
 from typing import Optional
 
 from sqlalchemy import select
+from sqlalchemy.ext.asyncio import AsyncSession
 
-from app.core.db import AsyncSessionLocal
 from app.models.meeting_room import MeetingRoom
 from app.schemas.meeting_room import MeetingRoomCreate
 
 
-async def create_meeting_room(new_room: MeetingRoomCreate) -> MeetingRoom:
+async def create_meeting_room(new_room: MeetingRoomCreate,
+                              session: AsyncSession) -> MeetingRoom:
     new_room_data = new_room.dict()
     db_room = MeetingRoom(**new_room_data)
 
-    async with AsyncSessionLocal() as session:
-        session.add(db_room)
-
-        await session.commit()  # Записываем изменения непосредственно в БД.
-        await session.refresh(db_room)  # Обновляем объект db_room: считываем данные из БД, чтобы получить его id.
+    session.add(db_room)
+    await session.commit()  # Записываем изменения непосредственно в БД.
+    await session.refresh(db_room)  # Обновляем объект db_room: считываем данные из БД, чтобы получить его id.
 
     return db_room
 
 
-async def get_room_id_by_name(room_name: str) -> Optional[int]:
-    async with AsyncSessionLocal() as session:
-        db_room_id = await session.execute(
-            select(MeetingRoom.id).where(MeetingRoom.name == room_name))
+async def get_room_id_by_name(room_name: str,
+                              session: AsyncSession) -> Optional[int]:
 
-        db_room_id = db_room_id.scalars().first()
+    db_room_id = await session.execute(
+        select(MeetingRoom.id).where(MeetingRoom.name == room_name))
+
+    db_room_id = db_room_id.scalars().first()
 
     return db_room_id
+
+
+async def read_all_rooms_from_db(session: AsyncSession) -> list[MeetingRoom]:
+
+    db_rooms = await session.execute(select(MeetingRoom))
+
+    return db_rooms.scalars().all()
