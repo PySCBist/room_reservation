4994ffb8bc0b0ffba2b4b8ff34e2b634bfe5f188
PySCBist <shds-2111@yandex.ru>
2024-06-14 21:54:24 +0300

    добавлена возможность редактирования существующей переговорки (patch)

--- app/schemas/meeting_room.py
+++ app/schemas/meeting_room.py
@@ -1,15 +1,25 @@
 from typing import Optional
 
-from pydantic import BaseModel, Field
+from pydantic import BaseModel, Field, validator
 
 
 class MeetingRoomBase(BaseModel):
-    name: Optional[str] = Field(..., min_length=3, max_length=100)
+    name: Optional[str] = Field(None, min_length=3, max_length=100)
     description: Optional[str]
 
 
 class MeetingRoomCreate(MeetingRoomBase):
     name: str = Field(..., min_length=3, max_length=100)
+
+
+class MeetingRoomUpdate(MeetingRoomBase):
+
+    @validator('name')
+    def name_cannot_be_null(cls, value: str):
+        if value is None:
+            raise ValueError('Нельзя оставлять название '
+                             'переговорки пустым!')
+        return value
 
 
 class MeetingRoomDB(MeetingRoomCreate):
